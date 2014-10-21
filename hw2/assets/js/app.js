7b688cd2edfc642b495a660910f1c518c7034eb1
Helen <b00705036@ntu.edu.tw>
2014-10-21 17:31:48 +0800

    done

--- hw2/assets/js/app.js
+++ hw2/assets/js/app.js
@@ -13,6 +13,7 @@ MyApp.controller('BoardCtrl',function($scope, $http){
 	$scope.main = {};
 	$scope.main.page = "index";
 	$scope.predicate = "issue_id";
+	$scope.reverse = true;
 
 	$scope.$watch('$viewContentLoaded', function(e){
 		$scope.viewissue();
